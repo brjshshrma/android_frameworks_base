92719f27d51aa0c133349492992af9e76babd1e6
Alex Cruz <mazdarider23@gmail.com>
2016-11-28 05:11:00 +0100

    QS tile titles visibility [1/2]
    ----
    abc ezio84:
    don't call textVisibility on each handleStateChanged
    but expose it through TileLayout mRecords and call it only when
    changing the settings
    
    fix also title visibility not being refreshed after a
    configuration change
    ----
    
    Pretty simple mod to remove tile titles if and when the titles become
    something that you don't want. A good case for this is if you set 7 or more
    columns across, the titles move down to 2 lines and is just looks awful IMO
    
    Before - https://i.imgur.com/G3ICaqb.png
    After - https://i.imgur.com/Hb0J1XW.png
    
    PS2 - Thanks to @maxwen for the know how to adjust the gap when titles
    are removed
    
    Change-Id: I1d7d336f127b200a97f2b6e1c38adc50988f651a
    Signed-off-by: Brajesh <brajesh.sharma87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6273,6 +6273,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_COLUMNS_LANDSCAPE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_TILE_TITLE_VISIBILITY),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -6289,7 +6292,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         public void update() {
             setFpToDismissNotifications();
             setForceAmbient();
-            setQsRowsColumns();
+            updateQsPanelResources();
         }
     }
 
@@ -6313,7 +6316,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mAmbientMediaPlaying != 0 && mAmbientIndicationContainer != null;
     }
 
-    private void setQsRowsColumns() {
+    private void updateQsPanelResources() {
         if (mQSPanel != null) {
             mQSPanel.updateResources();
         }

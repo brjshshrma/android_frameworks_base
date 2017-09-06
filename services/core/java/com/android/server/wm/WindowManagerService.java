a6210aa7370362dce71863f3a69df711b9d3f15d
bigrushdog <randall.rushing@gmail.com>
2017-09-06 20:46:51 +0200

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to AbcUtils
    
    Change-Id: If71944466c703a9b3fe54aea6bb21150b0a082bb

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6427,7 +6427,11 @@ public class WindowManagerService extends IWindowManager.Stub
     @Override
     public boolean needsNavigationBar() {
         return mPolicy.needsNavigationBar();
-    }
+   }
+
+    public void sendCustomAction(Intent intent) {
+        mPolicy.sendCustomAction(intent);
+    
 
     @Override
     public void lockNow(Bundle options) {

742883dee28ffa80765390672c520d6e9f5e3314
Brett Rogers <brettrogers11@gmail.com>
2017-10-16 16:32:24 +0200

    Status bar notification ticker
    abc ezio84: cleanup and port to Oreo
    du @nychitman1: cleanup and port to Nougat
    
    Change-Id: Idfc5c3a9398d0590ea0ef32c8a6efbca8513a1e8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4142,6 +4142,12 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
+         * Whether to show the notification ticker on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_TICKER = "status_bar_show_ticker";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

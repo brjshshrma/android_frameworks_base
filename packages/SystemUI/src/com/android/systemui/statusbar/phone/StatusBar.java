41fb6199c914068a6623b94d981db459be817c5f
ezio84 <brabus84@gmail.com>
2018-04-15 11:02:35 +0200

    Ambient and statusbar media tickers: code fixes and improvements
    - fix glitches (text disappearing then showing again) on track changes
    when ambient ticker is already showing
    - skip useless visibility calls
    - be sure to get the latest updated track infos after
    StatusBar.onAsyncInflationFinished() is finished for the
    media notification
    - be sure to remove track infos if the media notification is removed
    - cleanup code
    
    Change-Id: I888779951a1c9634b20da3dde1c6663051b8b197

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -744,6 +744,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 getMediaControllerPlaybackState(mMediaController)) {
             tickTrackInfo(mMediaController);
         } else {
+            mEntryToRefresh = null;
             if (isAmbientContainerAvailable()) {
                 ((AmbientIndicationContainer)mAmbientIndicationContainer).hideIndication();
             }
@@ -771,7 +772,11 @@ public class StatusBar extends SystemUI implements DemoMode,
         for (int i = 0; i < N; i++) {
             final Entry entry = activeNotifications.get(i);
             if (entry.notification.getPackageName().equals(pkg)) {
-                if (isAmbientContainerAvailable()) {
+               // NotificationInflater calls async MediaNotificationProcessoron to create notification
+                // colors and when finished will trigger AsyncInflationFinished for all registered callbacks
+                // like StatusBar. From there we'll send updated colors to Pulse
+                mEntryToRefresh = entry;}
+                  if (isAmbientContainerAvailable()) {
                     ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
                 }
                 break;
@@ -1992,7 +1997,17 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateNotificationShade();
         }
         entry.row.setLowPriorityStateUpdated(false);
-    }
+
+
+            if (mEntryToRefresh == entry) {
+            final Notification n = entry.notification.getNotification();
+            if (mTickerEnabled == 2) {
+                tick(entry.notification, true, true, mMediaMetadata);
+            }
+            if (isAmbientContainerAvailable()) {
+                ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
+            }
+ }
 
     private boolean shouldSuppressFullScreenIntent(String key) {
         if (isDeviceInVrMode()) {
@@ -2663,9 +2678,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                         + mMediaController.getPackageName());
             }
             mMediaController.unregisterCallback(mMediaListener);
-            setMediaPlaying();
         }
         mMediaController = null;
+        setMediaPlaying();
     }
 
     private boolean sameSessions(MediaController a, MediaController b) {
@@ -6162,7 +6177,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     }
                     setCleanLayout(mAmbientMediaPlaying == 3 ? reason : -1);
                     if (isAmbientContainerAvailable()) {
-                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setTickerMarquee(true);
+                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setPulsing(true);
                     }
                 }
 
@@ -6172,7 +6187,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     setPulsing(null);
                     setCleanLayout(-1);
                     if (isAmbientContainerAvailable()) {
-                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setTickerMarquee(false);
+                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setPulsing(false);
                     }
                 }
 

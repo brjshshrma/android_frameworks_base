83cd6348a5a8472d62618c50f21ca1d5cbe84f8e
simrat39 <simrats169169@gmail.com>
2018-06-28 05:13:18 +0000

    Revert "Add membar to recents options [1/2]"
    This reverts commit 682a87b67f0e0d305bcec2fd2e1d18f3f6bf8fc6.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4142,11 +4142,6 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
-         *  @hide
-          */
-        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

14fa421dead78cb7800179d0df9cc0f221e5995b
Joey <joey@lineageos.org>
2018-07-05 21:14:01 +0200

    systemUi: styles: support more dark overlays
    Change-Id: I4fafa2b1e342608e2690c3dda7cb06aaf88247ba
    Signed-off-by: Joey <joey@lineageos.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -287,6 +287,7 @@ import java.util.Stack;
 
 import lineageos.hardware.LiveDisplayManager;
 import lineageos.providers.LineageSettings;
+import lineageos.style.StyleInterface;
 
 public class StatusBar extends SystemUI implements DemoMode,
         DragDownHelper.DragDownCallback, ActivityStarter, OnUnlockMethodChangedListener,
@@ -3218,8 +3219,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     public boolean isUsingDarkTheme() {
         OverlayInfo systemuiThemeInfo = null;
         try {
-            systemuiThemeInfo = mOverlayManager.getOverlayInfo("org.lineageos.overlay.dark",
-                    mCurrentUserId);
+            String darkTheme = getDarkOverlay();
+            systemuiThemeInfo = mOverlayManager.getOverlayInfo(darkTheme, mCurrentUserId);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
@@ -3236,6 +3237,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             Log.w(TAG, e.getMessage());
         }
         return false;
+    }
+
+    private String getDarkOverlay() {
+        return LineageSettings.System.getString(mContext.getContentResolver(),
+                LineageSettings.System.BERRY_DARK_OVERLAY,
+                StyleInterface.OVERLAY_DARK_DEFAULT);
     }
 
     @Nullable
@@ -5229,8 +5236,8 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         if (isUsingDarkTheme() != useDarkTheme) {
             try {
-                mOverlayManager.setEnabled("org.lineageos.overlay.dark",
-                        useDarkTheme, mCurrentUserId);
+                String darkOverlay = getDarkOverlay();
+                mOverlayManager.setEnabled(darkOverlay, useDarkTheme, mCurrentUserId);
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
             }
